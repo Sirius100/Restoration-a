362b9473802e1a133ffe6f8886f433b6b4514452
ESA <esaul-00@mail.ru>
2021-05-22 17:40:09 +0300

    100% create for mozilla

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,6 @@ import clearcss from './disableCss.js';
 import request_menu from './menu.js';
 import  {item_elem, userAgent, odd_even} from './userAgent.js';
 
-userAgent();
-clearcss();
+// userAgent();
+// clearcss();
 request_menu();
